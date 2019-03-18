fc293ce58e387b8f9042a973ed8af2ef36b7dc2e
zhangcan <zhangcan@xiaoshouyi.com>
2019-03-18 13:46:08 +0800

    实现一个任意10进制数转2进制展示的方法，用位运算实现（只考虑正整数情况）

--- binarydemo/main.go
+++ binarydemo/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
+/**
+实现一个任意10进制数转2进制展示的方法，用位运算实现
+*/
+
 func main() {
-	
+	fmt.Println(tenToTwoBinaryString(99))
+}
+
+func tenToTwoBinaryString(num int) string {
+	result := ""
+	cur := num
+	for cur > 0 {
+		result = result + strconv.Itoa((cur)&1)
+		cur = cur >> 1
+	}
+	return result
 }
