8fe6ef1cd0ad775a842e2457be92b239cf5a0f0c
zhangcan <zhangcan@xuaoshouyi.com>
2019-03-19 22:39:27 +0800

    丑陋的完成了负数10进制转二进制的逻辑

--- binarydemo/main.go
+++ binarydemo/main.go
@@ -13,27 +13,43 @@ func main() {
 	fmt.Println(tenToTwoBinaryStringForInt(-99))
 }
 
-func tenToTwoBinaryStringForUInt(num int) string {
+func tenToTwoBinaryStringForInt(num int) string {
 	result := ""
+	arr := make([]int, 0, 1)
 	cur := num
+	if num < 0 {
+		cur = -num
+	}
+
 	for cur > 0 {
-		result = result + strconv.Itoa((cur)&1)
+		arr = append(arr, (cur)&1)
 		cur = cur >> 1
+	}
+
+	if num < 0 {
+		arr = reverse(arr)
+		arr = addOne(arr)
+	}
+
+	for _, bit := range arr {
+		result += strconv.Itoa(bit)
 	}
 	return result
 }
 
-func tenToTwoBinaryStringForInt(num int) string {
-	if num < 0 {
-		var unum = -num
-		result := ""
-		cur := unum
-		cur = ((cur >> 1) << 1) | ((cur & 1) ^ 1)
-		for cur > 0 {
-			result = result + strconv.Itoa((((cur) & 1) ^ 1))
-			cur = cur >> 1
-		}
-		return result
+func reverse(arr []int) []int {
+	var newArr = make([]int, 0, 0)
+	for _, bit := range arr {
+		newArr = append(newArr, ^bit&1)
 	}
-	return tenToTwoBinaryStringForUInt(num)
+	return newArr
+}
+
+func addOne(arr []int) []int {
+	var need = true
+	for index := len(arr) - 1; index > -1 && need; index-- {
+		need = (arr[index]&1 == 1)
+		arr[index] = arr[index] | 1
+	}
+	return arr
 }
