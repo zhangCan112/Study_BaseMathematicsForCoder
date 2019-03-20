85c8d4a1f10ed63a838bfa6cf2bd73524f33e68c
zhangcan <zhangcan@xuaoshouyi.com>
2019-03-21 07:42:03 +0800

    完成迭代法开平方根

--- lesson_3_iterative_method/main.go
+++ lesson_3_iterative_method/main.go
@@ -4,13 +4,31 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("main.go")
+	fmt.Println(squareRoot(99, 0.001))
 }
 
-func squareRoot(num float64) float64 {
+func squareRoot(num float64, precision float64) float64 {
 
-	return 1
+	var start float64
+	var end = num
+	var result = (end + start) / 2.0
+	for result > (1 + precision) {
+		fmt.Println(result)
+		if (math.Abs((result * result) - num)) < precision {
+			return result
+		} else if (result * result) > num {
+			end = result
+		} else {
+			start = result
+		}
+		result = (end + start) / 2.0
+	}
+
+	return result
 }
