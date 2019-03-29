082894b52dfa647e46a8a5dbf50ff2e57618a3f0
zhangcan <zhangcan@xuaoshouyi.com>
2019-03-29 22:42:01 +0800

    完成编辑距离的计算方法

--- lesson_9_dynamic_programming/q0/main.go
+++ lesson_9_dynamic_programming/q0/main.go
@@ -5,28 +5,60 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 ///可用策略
 const (
-	ADD     = iota //添加一个字段
+	ADD     = iota //添加一个字符
 	DELETE         //删除一个字符
 	REPLACE        //替换一个字符
 )
 
 func main() {
-	dynamicProgramming("张灿1221", "123")
+	var editDistance = dynamicProgramming("添加一个字符", "替换一个字符")
+	fmt.Println(editDistance)
 }
 
 func dynamicProgramming(src string, target string) int {
 	srcArr := []rune(src)
 	targetArr := []rune(target)
-	matirx := make([][]int, len(srcArr))
-	for i := 0; i < len(srcArr); i++ {
-		matirx = append(matirx, make([]int, len(targetArr)))
+
+	//初始化一个二位数组
+	matirx := make([][]int, len(srcArr)+1)
+	for i := 0; i <= len(srcArr); i++ {
+		matirx[i] = make([]int, len(targetArr)+1)
 	}
 
-	matirx[2][2] = 1123
+	//初始化数据
+	for i := 0; i <= len(srcArr); i++ {
+		matirx[i][0] = i
+	}
+	for j := 0; j <= len(targetArr); j++ {
+		matirx[0][j] = j
+	}
+
+	for i := 1; i <= len(srcArr); i++ {
+		for j := 1; j <= len(targetArr); j++ {
+			first := matirx[i-1][j] + 1
+			second := matirx[i][j-1] + 1
+			flag := 1
+			if srcArr[i-1] == targetArr[j-1] {
+				flag = 0
+			}
+			third := matirx[i-1][j-1] + flag
+			matirx[i][j] = min(first, second, third)
+		}
+	}
+
 	fmt.Println(matirx)
-	return 1
+	return matirx[len(srcArr)][len(targetArr)]
+}
+
+func min(first int, second int, third int) int {
+	min := math.Min(float64(first), float64(second))
+	min = math.Min(min, float64(third))
+	return int(min)
 }
